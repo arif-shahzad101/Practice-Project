b469d7bac0cfdc3fe1c3badecf50273cca2dea23
Arif <arifedwardian9999@gmail.com>
2023-11-24 17:47:50 +0500

    textutilis starting commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+import "./App.css";
 
 const App = () => {
-  return <div>hello world</div>;
+  return (
+    <div>
+      <Navbar />
+      <Main />
+    </div>
+  );
 };
 
 export default App;
